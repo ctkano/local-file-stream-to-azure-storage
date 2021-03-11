93f8c4f653c1dda88bf378b29dde63b7d344bebe
Cleyton Kano <cleyton.kano@thomsonreuters.com>
2021-03-11 16:36:18 -0300

    Update Microsoft Library References
    Update Microsoft Library References from the deprecated package "MicrosoftAzure.Storage" to "Azure.Storage.Blobs".

--- AzureStreamer/Stream.cs
+++ AzureStreamer/Stream.cs
@@ -1,12 +1,6 @@
-﻿using AzureStreamer.Objects;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.WindowsAzure.Storage.Blob;
+﻿using Azure.Storage.Blobs;
+using AzureStreamer.Objects;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzureStreamer
 {
@@ -16,54 +10,41 @@ namespace AzureStreamer
         /// Streaming to Azure Blob Container using Shared Access Signature URI (SAS URI)
         /// </summary>
         /// <param name="sas_uri">Shared Access Signature URI (SAS URI)</param>
-        /// <param name="blob">Blob Name / Path</param>
+        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container</param>
         /// <param name="file_path">Local File Path with File Name and Extension</param>
-        public static void SharedAccessSignatureURI(string sas_uri, string blob, string file_path)
+        public static void SharedAccessSignatureURI(string sas_uri, string blob_path, string file_path)
         {
-            //Azure storage access information
-            AzureAccessObjects azureAccessObjects = SegretateSASURI(sas_uri);
-
-            ///Storage credentials
-            StorageCredentials credentials = new StorageCredentials(azureAccessObjects.Token);
-
             //Blob container
-            CloudBlobContainer container = new CloudBlobContainer(new Uri(azureAccessObjects.URI), credentials);
+            BlobContainerClient blobContainer = new BlobContainerClient(new Uri(sas_uri));
 
-            //File name and extension or file path for Azure Blob Storage
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blob);
+            //File Name with extension or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container
+            BlobClient blobClient = blobContainer.GetBlobClient(blob_path);
 
             //Create or overwrite the blob with contents from the local file
             using (var fileStream = System.IO.File.OpenRead(file_path))
             {
-                blockBlob.UploadFromStream(fileStream);
+                blobClient.Upload(fileStream);
             }
         }
 
         /// <summary>
         /// Streaming to Azure Blob Container using Storage Account's Connection String
         /// </summary>
-        /// <param name="storage_account">Azure Storage Account Name</param>
-        /// <param name="blob_container">Blob Container Name</param>
-        /// <param name="blob">Blob Name / Path</param>
+        /// <param name="blob_container_name">Azure Blob Container Name</param>
+        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container</param>
         /// <param name="file_path">Local File Path</param>
-        public static void ConnectionString(string storage_account, string blob_container, string blob, string file_path)
+        public static void ConnectionString(string connection_string, string blob_container_name, string blob_path, string file_path)
         {
-            //Connection string from the storage account
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storage_account);
+            //Blob container
+            BlobContainerClient blobContainer = new BlobContainerClient(connection_string, blob_container_name);
 
-            //Blob client
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            //Blob container name
-            CloudBlobContainer container = blobClient.GetContainerReference(blob_container);
-
-            //File name and extension or file path for Azure Blob Storage
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blob);
+            //File Name with extension or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container
+            BlobClient blobClient = blobContainer.GetBlobClient(blob_path);
 
             //Create or overwrite the blob with contents from the local file
             using (var fileStream = System.IO.File.OpenRead(file_path))
             {
-                blockBlob.UploadFromStream(fileStream);
+                blobClient.Upload(fileStream);
             }
         }
 
