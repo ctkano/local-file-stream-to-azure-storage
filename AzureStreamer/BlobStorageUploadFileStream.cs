36889eba367f59fdaee06e6c59ef6394f82a3df8
Cleyton Kano <cleyton.kano@thomsonreuters.com>
2021-03-12 11:16:24 -0300

    Update methods to allow file overwrite in the blob storage

--- AzureStreamer/BlobStorageUploadFileStream.cs
+++ AzureStreamer/BlobStorageUploadFileStream.cs
@@ -6,12 +6,14 @@ namespace AzureStreamer
     public class BlobStorageUploadFileStream
     {
         /// <summary>
-        /// Streaming to Azure Blob Container using Shared Access Signature URI (SAS URI)
+        /// Upload file stream to Azure Blob Container using Shared Access Signature URI (SAS URI)
         /// </summary>
-        /// <param name="sas_uri">Shared Access Signature URI (SAS URI)</param>
-        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container</param>
-        /// <param name="file_path">Local File Path with File Name and Extension</param>
-        public static void SharedAccessSignatureURI(string sas_uri, string blob_path, string file_path)
+        /// <param name="sas_uri">Shared Access Signature URI (SAS URI).</param>
+        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container.</param>
+        /// <param name="file_path">Local File Path with File Name and Extension.</param>
+        /// <param name="allow_overwrite">Allow file overwrite in case of existance in the blob storage? When false, if a blob_path alread exists in the blob storage, it will return the following Status: 409 (The specified blob already exists.) and ErrorCode: BlobAlreadyExists. When true, the existing blob_path will be overwritten in the blob storage and no error is returned.</param>
+        /// <returns>Success Flag</returns>
+        public static bool SharedAccessSignatureURI(string sas_uri, string blob_path, string file_path, bool allow_overwrite)
         {
             //Blob container
             BlobContainerClient blobContainer = new BlobContainerClient(new Uri(sas_uri));
@@ -22,17 +24,22 @@ namespace AzureStreamer
             //Create or overwrite the blob with contents from the local file
             using (var fileStream = System.IO.File.OpenRead(file_path))
             {
-                blobClient.Upload(fileStream);
+                blobClient.Upload(fileStream, overwrite: allow_overwrite);
             }
+
+            return true;
         }
 
         /// <summary>
-        /// Streaming to Azure Blob Container using Storage Account's Connection String
+        /// Upload file stream to Azure Blob Container using Storage Account's Connection String
         /// </summary>
-        /// <param name="blob_container_name">Azure Blob Container Name</param>
-        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container</param>
-        /// <param name="file_path">Local File Path</param>
-        public static void ConnectionString(string connection_string, string blob_container_name, string blob_path, string file_path)
+        /// <param name="connection_string">Storage Account's Connection String.</param>
+        /// <param name="blob_container_name">Azure Blob Container Name.</param>
+        /// <param name="blob_path">File Name and/or Path with Virtual Directory and File Name with Extension, that will be created in the Blob Container.</param>
+        /// <param name="file_path">Local File Path.</param>
+        /// <param name="allow_overwrite">Allow file overwrite in case of existance in the blob storage? When false, if a blob_path alread exists in the blob storage, it will return the following Status: 409 (The specified blob already exists.) and ErrorCode: BlobAlreadyExists. When true, the existing blob_path will be overwritten in the blob storage and no error is returned.</param>
+        /// <returns>Success Flag</returns>
+        public static bool ConnectionString(string connection_string, string blob_container_name, string blob_path, string file_path, bool allow_overwrite)
         {
             //Blob container
             BlobContainerClient blobContainer = new BlobContainerClient(connection_string, blob_container_name);
@@ -43,8 +50,10 @@ namespace AzureStreamer
             //Create or overwrite the blob with contents from the local file
             using (var fileStream = System.IO.File.OpenRead(file_path))
             {
-                blobClient.Upload(fileStream);
+                blobClient.Upload(fileStream, overwrite: allow_overwrite);
             }
+
+            return true;
         }
     }
 }
